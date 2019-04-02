b0c28927ea3999d815ba24b66da4becf74218763
Sudhir <sudhiryelikar@gmail.com>
2019-04-02 23:46:05 +0530

    Adding hello world example for spring boot, react, vue, angular

--- spring-boot-react-hibernate-with-h2-full-stack/backend-spring-boot-jpa-hibernate-with-h2-full-stack/src/main/java/com/in28minutes/fullstack/springboot/jpa/hibernate/springbootjpahibernatewithh2fullstack/course/CourseResource.java
+++ spring-boot-react-hibernate-with-h2-full-stack/backend-spring-boot-jpa-hibernate-with-h2-full-stack/src/main/java/com/in28minutes/fullstack/springboot/jpa/hibernate/springbootjpahibernatewithh2fullstack/course/CourseResource.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
+@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200", "http://localhost:8081" })
 @RestController
 public class CourseResource {
 
